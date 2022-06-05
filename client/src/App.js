16312a933090fd01d6463f19be6b666fe318afb4
Lucas <lucaslimasimes@yahoo.com.br>
2022-06-05 17:21:00 -0300

    Conexão com o backend e verificação de login

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,33 @@
+import React, {useState, useEffect} from 'react';
 import './styles.css';
+import Axios from 'Axios';
 
 function App() {
+  const [user, setUser] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleLogin = () => {
+    Axios.post("https://localhost:3001/login", {user: user, password: password},{
+      
+    }).then((response) =>{
+      if(response){
+        alert("Logado com sucesso");
+        /*href?*/
+      }
+    })
+  }
+
   return (
     <div className="container">
       <h1 className="title">[inserir nome]</h1>
       <div className="containerLogin">
-        <input type="text" placeholder="Usuário"></input>
-        <input type="text" placeholder="Senha"></input>
-        <button className="buttonEnter" onClick={alert("Botão pressionado")}>Entrar</button>
+        <input type="text" name="user" placeholder="Usuário" onChange={(e)=>{
+          setUser(e.target.value)
+        }}></input>
+        <input type="text" name="password" placeholder="Senha" onChange={(e)=>{
+          setPassword(e.target.value)
+        }}></input>
+        <button className="buttonEnter" onClick={handleLogin}>Entrar</button>
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,19 +1,52 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const db = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "password",
+    password: "",
     database: "tcc",
 });
 
-app.get("/", (req,res) =>{
-    db.query("INSERT INTO usuarios (user, password) VALUES ('teste', 'teste')", (err, result) =>{
-        res.send("Teste4");
-    });
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}))
+
+//Login e cadastro
+app.post("/cadastro", (req,res) => {
+    /*const user = 'admin'
+    const password = 'admin'
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        db.query("INSERT INTO usuarios (user, password) VALUES (?, ?)", [user, hash], (err, result) =>{
+            if(err){
+                console.log(err);
+            }else{
+            res.send("Página de Cadastro");
+        }});
+    })*/
 });
+
+app.post("/login", (req,res) =>{
+    const user = req.body.user;
+    const password = req.body.password;
+    db.query("SELECT * FROM usuarios WHERE user = ?", [user], (err, result) =>{
+        if (err){
+            console.log(err);
+        }
+        if (result.lenght>0){
+            bcrypt.compare(password, result[0].password, (err, result) =>{
+                if(result){
+                    res.send(result);
+                }
+            })
+        }
+    })
+})
 
 app.listen(3001, () => {
     console.log("rodando na porta 3001");
